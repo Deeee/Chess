c024164a411caac4c76713297f6bf3dc4f6023d1
Rohan Barman <Rohan1Barman@gmail.com>
2014-06-20 02:55:00 -0400

    findAttack - for pawns

---
+++ Chess/EasyBot.m
@@ -0,0 +1,13 @@
+//
+//  EasyBot.m
+//  Chess
+//
+//  Created by Rohan Barman on 6/20/14.
+//  Copyright (c) 2014 Liu Di. All rights reserved.
+//
+
+#import "EasyBot.h"
+
+@implementation EasyBot
+
+@end
---
+++ EasyBot.h
@@ -0,0 +1,13 @@
+//
+//  EasyBot.h
+//  Chess
+//
+//  Created by Rohan Barman on 6/20/14.
+//  Copyright (c) 2014 Liu Di. All rights reserved.
+//
+
+#import "Board.h"
+
+@interface EasyBot : Board
+
+@end
